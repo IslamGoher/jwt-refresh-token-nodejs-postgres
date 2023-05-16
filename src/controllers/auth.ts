e49440d6e03d2adcfb731f37762cfdbd3f966088
IslamGoher <islam.goher@nowpay.com>
2023-05-16 22:08:37 +0300

    [api/login] Finish the logic of API login
    - create the refresh token and set it to the cookies
    - create the access token and send it with the response

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -3,37 +3,61 @@ import { loginDTO } from "../types/auth";
 import { Validator } from "../helpers/validator";
 import { UserModel } from "../database/models/user";
 import { compare } from "bcrypt";
+import { AuthService } from "../services/auth";
 
 export class AuthController {
   public static async login(req: Request, res: Response) {
-    const reqData: loginDTO = req.body;
-    const validationResult = Validator.validateLoginReqData(reqData);
+    try {
+      const reqData: loginDTO = req.body;
+      const validationResult = Validator.validateLoginReqData(reqData);
 
-    if (!validationResult.status)
-      return res.json({
-        status: 400,
-        message: validationResult.message,
+      if (!validationResult.status)
+        return res.json({
+          status: 400,
+          message: validationResult.message,
+        });
+
+      const currentUser = await UserModel.getUserByEmail(reqData.email);
+
+      if (!currentUser)
+        return res.json({
+          status: 404,
+          message: "there's no user found with given email",
+        });
+
+      const isPasswordValid = await compare(
+        reqData.password,
+        currentUser.password!
+      );
+
+      if (!isPasswordValid)
+        return res.json({
+          status: 403,
+          message: "invalid password",
+        });
+
+      const refreshToken = await AuthService.createRefreshToken(
+        currentUser.user_id!
+      );
+      const accessToken = AuthService.createAccessToken(currentUser);
+
+      res.cookie("rToken", refreshToken, {
+        httpOnly: true,
+        sameSite: "strict",
       });
 
-    const currentUser = await UserModel.getUserByEmail(reqData.email);
-
-    if (!currentUser)
       return res.json({
-        status: 404,
-        message: "there's no user found with given email",
+        status: 200,
+        message: "user successfully login",
+        token: accessToken,
       });
 
-    const isPasswordValid = await compare(
-      reqData.password,
-      currentUser.password!
-    );
-
-    if (!isPasswordValid)
+    } catch (error) {
+      console.log(error);
       return res.json({
-        status: 403,
-        message: "invalid password"
+        status: 500,
+        message: "server error",
       });
-
-    
+    }
   }
 }
