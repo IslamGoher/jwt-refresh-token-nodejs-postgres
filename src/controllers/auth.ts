715364230d494331afb293ecc1ed24ebd637faa8
IslamGoher <islam.goher@nowpay.com>
2023-05-16 21:33:27 +0300

    [api/login] validate user password

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { loginDTO } from "../types/auth";
 import { Validator } from "../helpers/validator";
+import { UserModel } from "../database/models/user";
+import { compare } from "bcrypt";
 
 export class AuthController {
   public static async login(req: Request, res: Response) {
@@ -10,9 +12,28 @@ export class AuthController {
     if (!validationResult.status)
       return res.json({
         status: 400,
-        message: validationResult.message
+        message: validationResult.message,
       });
-    
+
+    const currentUser = await UserModel.getUserByEmail(reqData.email);
+
+    if (!currentUser)
+      return res.json({
+        status: 404,
+        message: "there's no user found with given email",
+      });
+
+    const isPasswordValid = await compare(
+      reqData.password,
+      currentUser.password!
+    );
+
+    if (!isPasswordValid)
+      return res.json({
+        status: 403,
+        message: "invalid password"
+      });
+
     
   }
 }
